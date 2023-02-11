3f7fa6aef61c4221b25093e7b5ad6a6a91c7e24b
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-02-11 18:42:23 +0000

    extraReducers

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,6 +1,14 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 // Create loadCommentsForArticleId here.
+export const loadCommentsForArticleId = createAsyncThunk(
+  "comments/fetchAllComments",
+  async (id) => {
+    const response = await fetch(`api/articles/${id}/comments`);
+    const json = await response.json();
+    return json;
+  }
+);
 
 // Create postCommentForArticleId here.
 
@@ -8,8 +16,49 @@ export const commentsSlice = createSlice({
   name: "comments",
   initialState: {
     // Add initial state properties here.
+    byArticleId: {},
+    isLoadingComments: false,
+    failedToLoadComments: false,
   },
   // Add extraReducers here.
+  extraReducers: {
+    [loadCommentsForArticleId.pending]: (state, action) => {
+      state.isLoadingComments = true;
+      state.failedToLoadComments = false;
+    },
+
+    [loadCommentsForArticleId.fulfilled]: (state, action) => {
+      state.byArticleId = {
+        [action.payload.articleId]: action.payload.comments,
+      };
+      state.isLoadingComments = false;
+      state.failedToLoadComments = false;
+    },
+
+    [loadCommentsForArticleId.rejected]: (state, action) => {
+      state.isLoadingComments = false;
+      state.failedToLoadComments = true;
+    },
+
+    // postCommmentForArticleId
+    // [postCommentForArticleId.pending]: (state, action) => {
+    //   state.createCommentIsPending = true;
+    //   state.failedToCreateComment = false;
+    // },
+
+    // [postCommentForArticleId.fulfilled]: (state, action) => {
+    //   const articleId = action.payload.articleId;
+    //   state.byArticleId[articleId].push(action.payload);
+
+    //   state.createCommentIsPending = false;
+    //   state.failedToCreateComment = false;
+    // },
+
+    // [postCommentForArticleId.rejected]: (state, action) => {
+    //   state.createCommentIsPending = false;
+    //   state.failedToCreateComment = true;
+    // },
+  },
 });
 
 export const selectComments = (state) => state.comments.byArticleId;
