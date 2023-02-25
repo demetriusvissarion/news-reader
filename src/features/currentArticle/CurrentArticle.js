4effe4207e80b59ef7d69db59e93d758a3307840
Decebal Demetrius Vissarion <36327813+demetriusvissarion@users.noreply.github.com>
2023-02-25 18:10:44 +0000

    missed one

--- src/features/currentArticle/CurrentArticle.js
+++ src/features/currentArticle/CurrentArticle.js
@@ -1,5 +1,6 @@
 // import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+// import { useDispatch } from "react-redux";
 import {
   selectCurrentArticle,
   isLoadingCurrentArticle,
